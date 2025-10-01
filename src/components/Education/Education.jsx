0d1e7e2afbb845253ce2beca6d654734ac4275df
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-10-01 10:55:21 -0300

    modificação nas medias queries

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -6,7 +6,7 @@ export function Education(){
         <div className="education">
         <h1>Educação</h1>
         <div className="educ-1">
-        <h3>Análise e desenvolvimento de sistemas - 2024 - cursando</h3>
+        <h3>Análise e Desenvolvimento de Sistemas (2024 - cursando)</h3>
         <p>Instituição: PUCRS | Pontifícia Universidade Católica do Rio Grande do Sul</p>
         <p>
             Principais disciplinas: Desenvolvimento web, Engenharia de software, Ciência de Dados e Inteligência artificial,
@@ -15,7 +15,7 @@ export function Education(){
         </div>
 
         <div className="educ-2">
-        <h3>Técnico em Informática Integrado ao Ensino Médio - 2020 - 2022</h3>
+        <h3>Técnico em Informática Integrado ao Ensino Médio (2020 - 2022)</h3>
         <p>Instituição: Escola Técnica Estadual 25 de Julho - Ijuí/RS</p>
         <p>Principais disciplinas: Lógica de programção, Redes, Manutenção de computadores, Desenvolvimento web</p>
         </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ import {Education} from '../Education/Education';
 import {Experience} from '../Experience/Experience';
 import {Projects} from '../Projects/Projects';
 
-export function Profile(){ 
+export function Profile(){  
     return (
        <> 
         <div className="profile"> 
